dafc8ec246e39ac636f5f7115dad3a176805a5bd
Eric Harris-Braun <eric@harris-braun.com>
2024-01-10 14:16:50 -0500

    update to hc 0.3

--- ui/src/AboutDialog.svelte
+++ ui/src/AboutDialog.svelte
@@ -71,7 +71,7 @@
 </script>
 
 
-<sl-dialog label="TalkingStickies!: UI v0.8.0 for DNA v0.7.0" bind:this={dialog} width={600} >
+<sl-dialog label="TalkingStickies!: UI v0.9.0 for DNA v0.8.0" bind:this={dialog} width={600} >
     <div class="about">
         <p>TalkingStickies! is a demonstration Holochain app built by the Holochain Foundation.</p>
         <p> <b>Developers:</b>
